304ac68df4d707464d4d446d9c2b5089071cc99c
Luis Reyna <luis.reyna@itexico.com>
2019-06-10 14:07:38 -0500

    [MODULE RESOLVER] Eject and module imports configuration.

--- src/shared/routes/app.routes.tsx
+++ src/shared/routes/app.routes.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { PrivateRouteHOC } from '../../hoc/private-route.hoc';
-import { PrivateAppContainer } from '../../containers/private-app.container';
-import { SignInPageContainer } from '../../containers/sign-in-page/sign-in-page.container';
+import { PrivateAppContainer } from '@containers/private-app.container';
+import { SignInPageContainer } from '@containers/sign-in-page/sign-in-page.container';
 
 export const AppRoutes = () => (
   <Switch>
