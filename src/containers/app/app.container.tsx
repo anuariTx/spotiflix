ac19ab3dc6a97c329b09956a733af9f9bc1e52a3
Anuar Jimenez <anuar.jimenez@itexico.com>
2019-06-11 09:18:10 -0500

    [MODULE-RESOLVER] [ADD] Shared folder as path on app.container

--- src/containers/app/app.container.tsx
+++ src/containers/app/app.container.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { routerHistory } from '../../shared/router.history';
-import { AppRoutes } from '../../shared/routes/app.routes';
+import { routerHistory } from '@shared/router.history';
+import { AppRoutes } from '@routes/app.routes';
 
 export const AppContainer = () => {
   return (
