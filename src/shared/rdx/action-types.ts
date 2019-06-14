ed063eb8b4dafb03024d1285270c28db189caa9c
Anuar Jimenez <anuar.jimenez@itexico.com>
2019-06-14 09:05:31 -0500

    [MERGE-CONFLICT] [SOLVE] Conflict fetched from lazy load new package commit.

--- src/shared/rdx/action-types.ts
+++ src/shared/rdx/action-types.ts
@@ -3,6 +3,8 @@ export const SIGN_OUT = 'SIGN_OUT';
 
 export const CHANGE_THEME = 'CHANGE_THEME';
 
+export const FETCH_DATA = 'FETCH_DATA';
+
 export const SET_ERROR = 'SET_ERROR';
 
 export const SET_POSTS = 'SET_POSTS';
